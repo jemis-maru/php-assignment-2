7dca9aee9a748e9e422c38f684a5a9851a4c3bba
jemis-maru <jemis.maru@bacancy.com>
2023-12-04 13:12:15 +0530

    Add practical-1

---
+++ first/components/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar navbar-light">
+        <div class="container-fluid justify-content-start gap-4">
+            <button onclick="toggleSidebar()" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="../index.php">My Website</a>
+        </div>
+    </nav>
+</header>
---
+++ first/js/script.js
@@ -0,0 +1,8 @@
+const toggleSidebar = () => {
+  const sidebar = document.getElementById('sidebar');
+  if (sidebar.style.display === "none") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
+}
